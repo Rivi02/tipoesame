f117306e9bbb3e9bc7ee408a81400f91658c11f9
Rivi02 <r.verderio@itsrizzoli.it>
2022-05-23 10:40:48 +0200

    Initial commit

---
+++ src/main/java/org/example/Elaborazione_Arraylist.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Elaborazione_Arraylist {
+    ArrayList<Prodotti> arr;
+    Elaborazione_Arraylist(){
+        arr=new ArrayList<Prodotti>();
+    }
+
+    void add_Prodotti(int id,int qta,String nomeProdotto,String descrizione,double costo){
+        Prodotti a=new Prodotti(id,qta,nomeProdotto,descrizione,costo);
+        arr.add(a);
+    }
+
+
+    void crea_lista(){
+        add_Prodotti(1,50,"Iphone 13","Telefono",926.32);
+        add_Prodotti(2,100,"Iphone 12","Telefono",776.99);
+        add_Prodotti(3,150,"Iphone 12 Max","Telefono",800);
+        add_Prodotti(4,200,"Iphone 11","Telefono",560.48);
+        add_Prodotti(5,250,"Iphone XR","Telefono",500.12);
+    }
+    String all(){
+        String msg="All: ";
+        for (int i = 0; i < arr.size(); i++) {
+            msg=msg+arr.get(i).toString();
+        }
+        return "All: "+msg;
+    }
+
+    String sum(){
+        String somma="";
+        double som=0;
+        for (int i = 0; i < arr.size(); i++) {
+            som=som+arr.get(i).costo;
+        }
+        somma=Double.toString(som);
+        return "Sum: "+somma;
+    }
+
+    String minorPrice(){
+        arr.sort((Prodotti p1, Prodotti p2)->(int) (p1.costo-p2.costo));
+        return "Minor price: "+arr.get(0).toString();
+    }
+
+    String sort(){
+        arr.sort((Prodotti p1, Prodotti p2)->(int) (p1.costo-p2.costo));
+        String msg="Sorted: ";
+        for (int i = 0; i < arr.size(); i++) {
+            msg=msg+arr.get(i).toString();
+        }
+        return msg;
+    }
+
+    String quantita(){
+        arr.sort((Prodotti p1, Prodotti p2)->(p2.qta-p1.qta));
+        return "Qta: "+arr.get(0).toString();
+    }
+}
